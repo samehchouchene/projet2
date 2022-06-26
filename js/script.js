dd8a2f94310db1ebc2e71e57312daa37c34241dc
samehchouchene <102901241+samehchouchene@users.noreply.github.com>
2022-06-26 09:26:37 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,232 @@
+if (document.readyState == 'loading'){
+  document.addEventListener('DOMContentLoaded',ready)
+}else{
+  ready()
+}
+function ready(){
+var removecartitembotton = document.getElementsByClassName('btn-danger')
+console.log(removecartitembotton)
+for(var i=0 ; i< removecartitembotton.length ;i++){
+  var button = removecartitembotton[i]
+  button.addEventListener('click', removeCartItem)
+      
+  }
+
+var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+for (var i=0; i< quantityInputs.length ; i++){
+  var input = quantityInputs[i]
+  input.addEventListener('change', quantityChanged)
+}
+var addToCartButtons = document.getElementsByClassName('shop-item-button')
+for (var i = 0 ; i < addToCartButtons.length ; i++) {
+  var button = addToCartButtons[i]
+  
+  button.addEventListener('click', addToCartClicked)
+}
+
+document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchaseClicked)
+}
+
+
+
+
+
+
+let img__slider = document.getElementsByClassName('img__slider');
+
+let etape = 0;
+
+let nbr__img = img__slider.length;
+
+let precedent = document.querySelector('.precedent');
+let suivant = document.querySelector('.suivant');
+
+function enleverActiveImages() {
+    for(let i = 0 ; i < nbr__img ; i++) {
+        img__slider[i].classList.remove('active');
+    }
+}
+
+suivant.addEventListener('click', function() {
+    etape++;
+    if(etape >= nbr__img) {
+        etape = 0;
+    }
+    enleverActiveImages();
+    img__slider[etape].classList.add('active');
+})
+
+precedent.addEventListener('click', function() {
+    etape--;
+    if(etape < 0) {
+        etape = nbr__img - 1;
+    }
+    enleverActiveImages();
+    img__slider[etape].classList.add('active');
+})
+
+setInterval(function() {
+    etape++;
+    if(etape >= nbr__img) {
+        etape = 0;
+    }
+    enleverActiveImages();
+    img__slider[etape].classList.add('active');
+}, 3000)
+
+
+
+
+
+function removeCartItem(event){
+
+  var buttonclicked =event.target
+  buttonclicked.parentElement.parentElement.remove()
+  updatcarttotal()
+
+
+}
+
+function quantityChanged(event){
+  var input=event.target
+  if(isNaN(input.value)|| input.value <=0 ){
+      input.value =1
+  }
+  updatcarttotal()
+}
+function purchaseClicked() {
+  alert('Thank you for your purchase!')
+  var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+  while(cartItemContainer.hasChildNodes()) {
+      cartItemContainer.removeChild(cartItemContainer.firstChild)
+  }
+  updatcarttotal()
+}
+
+
+
+
+function addToCartClicked(event) {
+  var button=event.target
+  var shopItem = button.parentElement.parentElement
+  var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+  var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+  var imagesrc = shopItem.getElementsByClassName('shop-item-image')[0].src
+ 
+ 
+  addItemToCart(title,price,imagesrc)
+  updatcarttotal()
+}
+
+function addItemToCart(title,price,imagesrc) {
+  var cartRow = document.createElement('div')
+
+  cartRow.classList.add('cart-row')
+  
+  
+
+  var cartItems = document.getElementsByClassName('cart-items')[0]
+  var cartItemNames = cartItems.getElementsByClassName('cart-item-title')
+  for (var i = 0; i < cartItemNames.length; i++) {
+      if (cartItemNames[i].innerText == title) {
+          alert('This item is already in your cart')
+          return
+      }
+  }
+
+  
+  var cartRowContents = `
+  <div class="cart-item cart-column">
+  <img class="cart-item-image" src="${imagesrc}" width="100" height="100">
+  <span class="cart-item-title">${title}</span>
+</div>
+<span class="cart-price cart-column">${price}</span>
+<div class="cart-quantity cart-column">
+  <input class="cart-quantity-input" type="number" value="1">
+  <button class="btn btn-danger" type="button">REMOVE</button>
+</div>
+  `
+  cartRow.innerHTML =cartRowContents
+  cartItems.append(cartRow)
+  cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click',removeCartItem)
+  cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change',quantityChanged)
+  
+}
+
+
+function updatcarttotal(){
+  
+      var cartRows = document.getElementsByClassName('cart-row')
+      var total = 0
+      for (var i = 0; i < cartRows.length; i++) {
+          var cartRow = cartRows[i]
+          var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+          var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+          if (priceElement == null || quantityElement == null) continue
+          var price = parseFloat(priceElement.innerText.replace('$', ''))
+          var quantity = parseInt(quantityElement.value)
+          total += price * quantity
+      }
+  
+      document.getElementsByClassName('cart-total-price')[0].innerText = '$' + Math.round(total * 100) / 100
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
+
+
+
+
+
+
+
+
+
+
+
+  
+   
+
+
+
